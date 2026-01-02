df588bb4ddb99fdc36d3f9a00448a23fa99702c1
Mahortin <nitramwolfrum@web.de>
2026-01-02 17:31:16 +0100

    update vite.config

--- vue-frontend/vite.config.js
+++ vue-frontend/vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: '/ischeroth-werterechner-vite/',
+  base: '/',
   server: {
     host: '0.0.0.0',
     port: 5173,
